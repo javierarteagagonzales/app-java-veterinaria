93337604380896712f3fd0c43b75cbb9a99af9e6
Javier <javalejan12@gmail.com>
2023-04-29 17:38:59 -0500

    Interfaces

---
+++ Veterinaria/src/main/java/com/mycompany/veterinaria/Veterinaria.java
@@ -0,0 +1,16 @@
+package com.mycompany.veterinaria;
+
+import com.mycompany.veterinaria.igu.Principal;
+
+/**
+ *
+ * @author javier arteaga gonzales
+ */
+public class Veterinaria {
+
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+    }
+}
